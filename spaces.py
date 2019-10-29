1487fac283aa642faee9ec174e2be5334a5c0a2c
jphayek <jphayek1@gmail.com>
2019-10-29 12:11:00 +0100

    modifc

--- spaces.py
+++ spaces.py
@@ -2,11 +2,32 @@
 import argparse
 from pathlib import Path
 
-parser = argparse.ArgumentParser(
-    description="Searches and replaces in filenames.")
-parser.add_argument("path", help="Root directory in which to search for files.")
-parser.add_argument("--search", default=" ", help="Character to search.")
-parser.add_argument("--replace", default="_", help="Character to use as a replacement.")
-parser.add_argument("--recurse", action="store_true", help="Give to replace recursively in directories.")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Searches and replaces in filenames.")
+    parser.add_argument("path", help="Root directory in which to search for files.")
+    parser.add_argument("--search", default=" ", help="Character to search.")
+    parser.add_argument(
+        "--replace", default="_", help="Character to use as a replacement."
+    )
+    parser.add_argument(
+        "--recurse",
+        action="store_true",
+        help="Give to replace recursively in directories.",
+    )
 
-print(parser.parse_args())
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    files = [
+        file
+        for file in Path(args.path).glob("*" if not args.recurse else "**/*")
+        if file.is_file()
+    ]
+    for file in files:
+        new_name = file.name.replace(args.search, args.replace)
+        file.rename(file.parent / new_name)
+
+
+main()
